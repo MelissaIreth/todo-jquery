f25b6040e1c40e885aba7d9969214cbc2a018677
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-06-29 10:37:29 -0400

    check and delete almost

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
-	$('#el_input').keydown(function(e) {
-	var key = e.which;
-	var msg = $("#el_input").val();
-	if (key == 13) {// As ASCII code for ENTER key is "13"
-		$(".tareas").append("<input type='checkbox' id='test5'>"+"<label for='test5'>" + msg + "</label><a class='btn-floating waves-effect waves-ligth'><i class='material-icons'>delete</i></a>")
-	}
-});
-});
 
+	$('#el_input').keydown(function(e){
+		var key = e.which;
+		var msg = $("#el_input").val();
+		if (key == 13) {// As ASCII code for ENTER key is "13"
+		$(".tareas").append("<input type='checkbox' id='test5' class='check'>"+"<label for='test5'>" + msg + "</label><a class='btn-floating waves-effect waves-ligth'><i class='material-icons delete'>delete</i></a>")
+		}
+
+		$('.check').click(function(){
+		$('.tareas').remove();
+		$('.tareascompletas').append(msg + "</label><a class='btn-floating waves-effect waves-ligth'><i class='material-icons delete1'>delete</i></a>")
+
+		});
+
+		$('.delete').click(function(){
+			$('.tareas').remove();
+		});
+
+		$('.delete1').click(function(){
+			$('.tareascompletas').remove();
+		});
+
+	});
+
+});
