7c1858feba49e329edce9dc028dac9faa5aa513f
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-06-29 11:04:24 -0400

    need correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-
+/*Acá se llama a lo ingresado en el input que al apretar 'enter' pase a nueva tarea con su checkbox y basurero*/
 	$('#el_input').keydown(function(e){
 		var key = e.which;
 		var msg = $("#el_input").val();
 		if (key == 13) {// As ASCII code for ENTER key is "13"
-		$(".tareas").append("<input type='checkbox' id='test5' class='check'>"+"<label for='test5'>" + msg + "</label><a class='btn-floating waves-effect waves-ligth'><i class='material-icons delete'>delete</i></a>")
+		$(".tareas").append("<input type='checkbox' id='test5' class='check'>"+"<label for='test5'>" + msg + "</label><a class='right btn-floating waves-effect waves-ligth'><i class='material-icons delete'>delete</i></a>")
 		}
-
+/*Acá se le da la funcion al 'check' para que las tareas pasen a completadas*/
 		$('.check').click(function(){
 		$('.tareas').remove();
-		$('.tareascompletas').append(msg + "</label><a class='btn-floating waves-effect waves-ligth'><i class='material-icons delete1'>delete</i></a>")
+		$('.tareascompletas').append("<label for='test5'>"+msg + "</label><a class='right btn-floating waves-effect waves-ligth'><i class='material-icons delete1'>delete</i></a>")
 
 		});
-
+/*Acá se le da la funcion al basurero para que se elimine la tarea*/
 		$('.delete').click(function(){
 			$('.tareas').remove();
 		});
-
+/*Acá deberia ser para eliminar la tarea completada pero no me funciono*/
 		$('.delete1').click(function(){
 			$('.tareascompletas').remove();
 		});
